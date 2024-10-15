8cefd1142877356ad4e0561c193971929067f035
Julien <juju.gruet@mail.com>
2024-10-15 11:13:11 +0200

    end

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 const log = console.log;
-log(chalk.hex('#DEADED').bold('jax'));
-log(chalk.hex('#FFE4E1').bold('trynda'));
-log(chalk.hex('#FDF5E6').bold('Zac'));
-log(chalk.hex('#F0FFF0').bold('camille'));
+log(chalk.yellow('jax'));
+log(chalk.red('trynda'));
+log(chalk.green('Zac'));
+log(chalk.blue('camille'));
