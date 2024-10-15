563907335c3c6caf07f0ec6b1b6055e6fcaebb4b
Julien <juju.gruet@mail.com>
2024-10-15 11:00:33 +0200

    end

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+const log = console.log;
+log(chalk.hex('#DEADED').bold('jax'));
+log(chalk.hex('#FFE4E1').bold('trynda'));
+log(chalk.hex('#FDF5E6').bold('Zac'));
+log(chalk.hex('#F0FFF0').bold('camille'));
